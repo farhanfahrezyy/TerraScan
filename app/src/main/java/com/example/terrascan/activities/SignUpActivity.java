50ea27ee729c801d5afe32b974e2cf67e54b9d1b
FrzAdli <adlifarizi04@gmail.com>
2023-10-18 12:54:08 +0700

    update

--- app/src/main/java/com/example/terrascan/activities/SignUpActivity.java
+++ app/src/main/java/com/example/terrascan/activities/SignUpActivity.java
@@ -56,7 +56,7 @@ public class SignUpActivity extends AppCompatActivity {
                     preferenceManager.putString(Constants.KEY_USER_ID, documentReference.getId());
                     preferenceManager.putString(Constants.KEY_USERNAME, binding.inputUsername.getText().toString());
                     preferenceManager.putString(Constants.KEY_PHONE_NUMBER, binding.inputPhoneNumber.getText().toString());
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
                     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     startActivity(intent);
                 })
