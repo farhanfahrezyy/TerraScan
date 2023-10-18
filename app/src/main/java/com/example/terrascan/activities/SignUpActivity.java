2559ebd1a741186e277c24b3a4ba2e992846d6c1
FrzAdli <adlifarizi04@gmail.com>
2023-10-18 08:21:32 +0700

    update

--- app/src/main/java/com/example/terrascan/activities/SignUpActivity.java
+++ app/src/main/java/com/example/terrascan/activities/SignUpActivity.java
@@ -2,7 +2,11 @@ package com.example.terrascan.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.terrascan.databinding.ActivitySignUpBinding;
 
@@ -15,4 +19,52 @@ public class SignUpActivity extends AppCompatActivity {
         binding = ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
     }
+
+    private void setListeners() {
+        binding.layoutSignIn.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(), SignInActivity.class)));
+
+        binding.buttonSignUp.setOnClickListener(v -> {
+            if(isValidSignUpDetail()) {
+                signUp();
+            }
+        });
+    }
+
+    private void signUp() {
+
+    }
+
+    private void loading(Boolean isLoading) {
+        if(isLoading) {
+            binding.buttonSignUp.setVisibility(View.INVISIBLE);
+            binding.progessBar.setVisibility(View.VISIBLE);
+        } else {
+            binding.progessBar.setVisibility(View.INVISIBLE);
+            binding.buttonSignUp.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private Boolean isValidSignUpDetail() {
+        if(binding.inputEmail.getText().toString().trim().isEmpty()) {
+            showToast("Enter email");
+            return false;
+        } else if(!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString()).matches()) {
+            showToast("Enter valid email");
+            return false;
+        } else if(binding.inputPhoneNumber.getText().toString().trim().isEmpty()) {
+            showToast("Enter phone number");
+            return false;
+        } else if(binding.inputPassword.getText().toString().trim().isEmpty()) {
+            showToast("Enter password");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
 }
